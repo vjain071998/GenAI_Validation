e47d5a3805ab96c09d99913730429c1c719d455f
VishaL <vj199807@gmail.com>
2025-12-19 18:13:59 +0530

    6th Commit

--- outcome_analysis_agent/generate_spot_scenarios.py
+++ outcome_analysis_agent/generate_spot_scenarios.py
@@ -11,3 +11,17 @@ def spot_scenerio(base_spot, pct_range=0.20, step=0.05):
         scenarios.append({"S0": base_spot * m})
 
     return scenarios
+
+def strike_scenerio(base_strike, pct_range=0.20, step=0.05):
+    """
+    Creates multiple scenarios where only the strike price is bumped,
+    while all other parameters remain the same.
+    Example: -20%, -15%, ... +20%
+    """
+    scenarios = []
+    multipliers = [1 + i for i in [x * step for x in range(int(-pct_range/step), int(pct_range/step)+1)]]
+
+    for m in multipliers:
+        scenarios.append({"K": base_strike * m})
+
+    return scenarios
